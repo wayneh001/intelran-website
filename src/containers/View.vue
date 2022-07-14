83ee6fbef7fa95b82bb459e097de825f8519ca81
Wayne Huang <wayneh001@yahoo.com.tw>
2022-07-14 08:10:03 +0800

    Structure constructing
    1. Structure of pages.
    2. Structure of containers.

---
+++ src/containers/View.vue
@@ -0,0 +1,33 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import CNav from "@/components/CNav";
+import CHeader from "@/components/CHeader";
+import CDisplay from "@/components/CDisplay";
+import CTable from "@/components/CTable";
+import CFooter from "@/components/CFooter";
+import CAlert from "@/components/CAlert";
+export default {
+  name: "view",
+  components: [
+    { CNav },
+    { CHeader },
+    { CDisplay },
+    { CTable },
+    { CFooter },
+    { CAlert },
+  ],
+  props: {
+    content: {
+      type: String,
+      require: true,
+    },
+  },
+  data() {},
+  methods: {},
+};
+</script>
+
+<style></style>
---
+++ src/pages/smartHome/smartHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <View :content="content" />
+  </div>
+</template>
+
+<script>
+import View from "@/containers/view";
+export default {
+  name: "smartHome",
+  components: [
+    { View },
+  ],
+  data() {
+    return {
+        content: ""
+    }
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
